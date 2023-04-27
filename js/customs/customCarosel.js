9f8dd061e694ad9ae81db5fa1a69773e2ad9d0da
Dang Hieu <danghieutieumai@gmail.com>
2023-04-28 02:46:29 +0700

    done 3 page

--- js/customs/customCarosel.js
+++ js/customs/customCarosel.js
@@ -23,5 +23,25 @@ $(document).ready(function () {
       }
   }
   });
+  $(".owl-theme-3").owlCarousel({
+    items: 3,
+    loop: true,
+    responsive:{
+      0:{
+          items:1,
+          nav:true
+      },
+      600:{
+          items:2,
+          nav:false,
+      },
+      1000:{
+          items:3,
+          nav:false,
+          loop:false,
+          margin:10,
+      }
+  }
+  });
 });
 
