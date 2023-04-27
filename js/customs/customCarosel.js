a02564ee346fbd8425acafc1d109e00b4dab1238
Dang Hieu <danghieutieumai@gmail.com>
2023-04-28 00:04:14 +0700

    done page gta-download

--- js/customs/customCarosel.js
+++ js/customs/customCarosel.js
@@ -7,6 +7,21 @@ $(document).ready(function () {
   $(".owl-theme-2").owlCarousel({
     items: 3,
     loop: true,
+    responsive:{
+      0:{
+          items:1,
+          nav:true
+      },
+      600:{
+          items:2,
+          nav:false,
+      },
+      1000:{
+          items:3,
+          nav:false,
+          loop:false
+      }
+  }
   });
 });
 
