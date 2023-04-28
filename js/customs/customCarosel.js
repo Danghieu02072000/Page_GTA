b3d19e4c0c5b0ae287c6e5433a6f048b6373bafe
Dang Hieu <danghieutieumai@gmail.com>
2023-04-28 18:02:49 +0700

    done 3 page

--- js/customs/customCarosel.js
+++ js/customs/customCarosel.js
@@ -1,47 +1,49 @@
 $(document).ready(function () {
-  $(".owl-theme").owlCarousel({
-    items: 1,
-    loop: true,
-    nav: true,
-  });
-  $(".owl-theme-2").owlCarousel({
-    items: 3,
-    loop: true,
-    responsive:{
-      0:{
-          items:1,
-          nav:true
-      },
-      600:{
-          items:2,
-          nav:false,
-      },
-      1000:{
-          items:3,
-          nav:false,
-          loop:false
+      $(".owl-theme").owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+      });
+
+      $(".owl-theme-2").owlCarousel({
+        items: 3,
+        loop: true,
+        responsive:{
+          0:{
+              items:1,
+              nav:true
+          },
+          600:{
+              items:2,
+              nav:false,
+          },
+          1000:{
+              items:3,
+              nav:false,
+              margin: 5,
+              loop:true,
+          }
       }
-  }
-  });
-  $(".owl-theme-3").owlCarousel({
-    items: 3,
-    loop: true,
-    responsive:{
-      0:{
-          items:1,
-          nav:true
-      },
-      600:{
-          items:2,
-          nav:false,
-      },
-      1000:{
-          items:3,
-          nav:false,
-          loop:false,
-          margin:10,
+      });
+      $(".owl-theme-3").owlCarousel({
+        items: 3,
+        loop: true,
+        responsive:{
+          0:{
+              items:1,
+              nav:true
+          },
+          600:{
+              items:2,
+              nav:false,
+          },
+          1000:{
+              items:3,
+              nav:false,
+              loop:true,
+              margin:10,
+          }
       }
-  }
-  });
+      });
 });
 
